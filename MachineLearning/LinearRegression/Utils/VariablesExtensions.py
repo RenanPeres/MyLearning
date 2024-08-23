7324d8193442e7b2a547d2f8701729712ecab545
RenanPeres <renanperes99@usp.br>
2024-08-22 23:28:20 -0300

    Update linear regression study files

---
+++ MachineLearning/LinearRegression/Utils/VariablesExtensions.py
@@ -0,0 +1,20 @@
+from typing import Optional
+import pandas as pd
+
+class VariablesExtensions:
+    _REGRESSION_AGGREGATOR_SYMBOL = ' + '
+    
+    @staticmethod
+    def print_nominal_counts(dataFrame: pd.DataFrame, indexes: list):
+        for index in indexes:
+            print(dataFrame[index].value_counts())
+
+    @staticmethod
+    def get_regression_command_string(dataFrame: pd.DataFrame, target_index: str, columns_to_remove: Optional[list] = []) -> str:
+        variables = list(dataFrame.columns.values)
+        variables.remove(target_index)
+
+        for column in columns_to_remove:
+            variables.remove(column)
+
+        return target_index + ' ~ ' + VariablesExtensions._REGRESSION_AGGREGATOR_SYMBOL.join(variables)
